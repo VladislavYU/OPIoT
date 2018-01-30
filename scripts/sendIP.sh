8adffd5874b0616ef4c04c137f990172877ccec5
Владислав <velundzv@yandex.ru>
2018-01-30 21:59:48 +0500

    ip

--- scripts/sendIP.sh
+++ scripts/sendIP.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ip=orangepi
+ip=
+while IFS=$': \t' read -a line ;do
+    [ -z "${line%inet}" ] && ip=${line[${#line[1]}>4?1:2]} &&
+        [ "${ip#127.0.0.1}" ] && myip=$ip
+  done< <(LANG=C /sbin/ifconfig)
+echo $ip
 
 python /home/orangepi/OPIoT/scripts/send_msg.py -a $ip
