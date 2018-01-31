4033296e7bf8695097bd0128d0ae078811679f23
Владислав <velundzv@yandex.ru>
2018-01-31 22:20:19 +0500

    fix ip4

--- scripts/send_msg.py
+++ scripts/send_msg.py
@@ -4,13 +4,14 @@
 from optparse import OptionParser
 import locale
 from gsmmodem.modem import GsmModem
+import logging
 
 parser = OptionParser()
 parser.add_option("-a", "--address", action="store", dest="ip", type="string", help="Cureent ip address")
 
 PORT = '/dev/modem0'
 BAUDRATE = 115200
-PIN = None # SIM card PIN (if any)
+PIN = "0000" # SIM card PIN (if any)
 
 if __name__ == '__main__':
     encode = locale.getdefaultlocale()
@@ -19,11 +20,12 @@ if __name__ == '__main__':
         parser.print_help()
         exit()
 
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
     modem = GsmModem(PORT, BAUDRATE)
     modem.smsTextMode = True
-    modem.connect()
+    modem.connect(PIN)
 
-    modem.sendSms("+79227814419", options.ip)
+    modem.sendSms(+79227814419, options.ip)
 
     try:
         modem.rxThread.join(10)  # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
